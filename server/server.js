425bcd31e9e59251ad2dacae77f3f79471715a81
fayiztv <tvfayiztv@gmail.com>
2024-02-06 01:23:50 +0530

    db connected

--- server/server.js
+++ server/server.js
@@ -1,1 +1,7 @@
-console.log("this is from server");
+import express from "express";
+import dbConnect from "./database/connection.js";
+
+dbConnect();
+const app = express();
+app.use(express.json({limit: '50mb'}));
+app.listen(3000, () => console.log(`Server running on port 3000`));
